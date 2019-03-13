d919676d457f36eb8854d8d5668d7a8b698375f3
龙云 <longyun@co-mall.com>
2019-03-13 11:53:33 +0800

    playing digits

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 //mod push_dominoes;
 mod first_unique_character;
 mod give_diamond;
+mod playing_digits;
 
 fn main() {
 //    let test = String::from("a.L.R...LR..L..");
-    //    println!("{}", push_dominoes::push_dominoes(test));
-    //    println!("{}",first_unique_character::first_uniq_char(test));
-    let n:i32 = 5;
-    if let Some(s) = give_diamond::print(5){
-        println!("{}",s);
-    }
+//    println!("{}", push_dominoes::push_dominoes(test));
+//    println!("{}",first_unique_character::first_uniq_char(test));
+//    let n:i32 = 5;
+//    if let Some(s) = give_diamond::print(5){
+//        println!("{}",s);
+//    }
+    let n = 89;
+    let p = 1;
+    println!("{}", playing_digits::dig_pow(n, p));
 }
+
