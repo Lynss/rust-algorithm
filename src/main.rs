e03e31d16e8e24b2b35d03205a1a9abe32c0189b
龙云 <longyun@co-mall.com>
2019-03-26 18:23:11 +0800

    match_substitute

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate regex;
+
 //mod push_dominoes;
 //mod first_unique_character;
 //mod give_diamond;
@@ -8,7 +10,10 @@
 //mod move_square;
 //mod algebraic_list;
 //use algebraic_list::{Cons};
-mod recover_secret;
+//mod recover_secret;
+
+use regex::Regex;
+use std::collections::HashMap;
 
 fn main() {
     //    let test = String::from("a.L.R...LR..L..");
@@ -41,4 +46,56 @@ fn main() {
     //    println!("{:?}", Cons::from_iter(vec.clone()).map(|a|a+3).to_vec());
 
     //    recover_secret::rs_print();
+
+    let s1 = "Program title: Primes\nAuthor: Kern\nCorporation: Gold\nPhone: +1-503-555-0091\nDate: Tues April 9, 2005\nVersion: 6.7\nLevel: Alpha";
+    fn change(s: &str, prog: &str, version: &str) -> String {
+        let version = format!(" {}", version).to_owned();
+        let mut initial = s
+            .split('\n')
+            .map(|item| {
+                if let [key, value] = *item.split(':').collect::<Vec<_>>().as_slice() {
+                    (key, value)
+                } else {
+                    ("", "")
+                }
+            })
+            .collect::<HashMap<&str, &str>>();
+        dbg!(initial["Phone"].trim());
+        let phone_regex = Regex::new(r"^\+1-\d{3}-\d{3}-d{4}$").unwrap();
+        let version_regex = Regex::new(r"^\d+\.\d+$").unwrap();
+        dbg!(phone_regex.is_match(initial["Phone"].trim()));
+        dbg!(version_regex.is_match(initial["Version"].trim()));
+        if !phone_regex.is_match(initial["Phone"].trim()) || !version_regex.is_match(initial["Version"].trim()) {
+            return "ERROR: VERSION or PHONE".into();
+        };
+        initial.entry("Version").and_modify(|n| {
+            let nn = n.parse::<f64>().unwrap();
+            if nn > 2.0 {
+                *n = version.as_str();
+            };
+        });
+        initial.entry("Phone").and_modify(|p| {
+            *p = " +1-503-555-0090";
+        });
+        initial.entry("Author").and_modify(|p| {
+            *p = " g964";
+        });
+        initial.entry("date").and_modify(|p| {
+            *p = " 2019-01-01";
+        });
+        initial.entry("Program").or_insert(prog);
+        initial.remove("Corporation");
+        initial.remove("Level");
+        format!(
+            "Program:{} Author:{} Phone:{} Date:{} Version:{}",
+            initial["Program"],
+            initial["Author"],
+            initial["Phone"],
+            initial["Date"],
+            initial["Version"]
+        )
+    }
+    dbg!(
+        change(s1, "Ladder", " 1.1")
+    );
 }
