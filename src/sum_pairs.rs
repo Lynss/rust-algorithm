89051af4a419162379ba05d6b7773963f61854cb
龙云 <longyun@co-mall.com>
2019-03-28 11:53:13 +0800

    fmt

--- src/sum_pairs.rs
+++ src/sum_pairs.rs
@@ -4,7 +4,7 @@ fn sum_pairs(ints: &[i8], s: i8) -> Option<(i8, i8)> {
     let mut min = std::usize::MAX;
     let mut result: Option<(i8, i8)> = None;
     let mut markers = HashMap::new();
-    let mut index:usize = 0;
+    let mut index: usize = 0;
     for &x in ints {
         if min == 1 {
             break;
@@ -12,7 +12,7 @@ fn sum_pairs(ints: &[i8], s: i8) -> Option<(i8, i8)> {
         let d = s - x;
         if let Some(&v) = markers.get(&d) {
             if (index - v) < min {
-                min = index - v ;
+                min = index - v;
                 result = Some((d, x));
             }
         }
@@ -22,7 +22,7 @@ fn sum_pairs(ints: &[i8], s: i8) -> Option<(i8, i8)> {
     result
 }
 
-pub fn print_sum_pairs(){
+pub fn print_sum_pairs() {
     let test = [1, 2, 3, 1, 8, 6, 7, 3, 15];
     dbg!(sum_pairs(&test, 8));
 }
