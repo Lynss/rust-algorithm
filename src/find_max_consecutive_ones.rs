88a53b87768a90e3598856df023f8978c7ef3afa
longyun <ly1169134156@163.com>
2019-06-26 10:25:30 +0800

    find_max_consecutive_ones

---
+++ src/find_max_consecutive_ones.rs
@@ -0,0 +1,26 @@
+pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
+    use std::cmp;
+    let mut max_num = 0;
+    let mut counter = 0;
+    for current in nums {
+        if current == 1{
+            counter += 1;
+            max_num = cmp::max(max_num, counter);
+        }else{
+            counter = 0;
+        }
+    }
+    max_num
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::rc::Rc;
+    use std::cell::RefCell;
+
+    #[test]
+    fn test_find_max_consecutive_ones() {
+        assert_eq!(find_max_consecutive_ones(vec![1, 1, 0, 1, 1, 1]), 3);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ mod add_two_numbers;
 mod masking_personal_information;
 mod repeated_times;
 mod max_subarray_sum_circular;
+mod find_max_consecutive_ones;
 
 #[cfg(test)]
 mod tests {}
