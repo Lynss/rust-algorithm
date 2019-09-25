400bf109293f99f7d5d1f829c61a9c1891240850
longyun <ly1169134156@163.com>
2019-09-25 10:03:36 +0800

    remove dead_code warning

--- src/recent_counter.rs
+++ src/recent_counter.rs
@@ -9,12 +9,14 @@ struct RecentCounter {
  * If you need a mutable reference, change it to `&mut self` instead.
  */
 impl RecentCounter {
+    #[allow(dead_code)]
     fn new() -> Self {
         RecentCounter {
             queue: VecDeque::new(),
         }
     }
 
+    #[allow(dead_code)]
     fn ping(&mut self, t: i32) -> i32 {
         while !self.queue.is_empty() && self.queue[0] < t - 3000 {
             self.queue.pop_front();
